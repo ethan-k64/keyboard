1b4069b15dde62629446fe4315b99defe66719cd
ethan-k64 <59579977+ethan-k64@users.noreply.github.com>
2020-05-19 12:38:02 -0400

    Added a board file.
    The board file adds better UI.

---
+++ board.js
@@ -0,0 +1,60 @@
+// Declare New Sketch
+let board = function(p) {
+  let slider;
+
+  let sineButton;
+  let triangleButton;
+  let sawtoothButton;
+  let squareButton;
+
+  p.setup = function() {
+    const cnv = p.createCanvas(len * 60, 175);
+    cnv.position(displayWidth / 2 - width / 2, 0);
+
+    sineButton = p.createButton("Sine");
+    sineButton.position(1000, 75);
+    sineButton.mousePressed(sineType);
+
+    triangleButton = p.createButton("Triangle");
+    triangleButton.position(1100, 75);
+    triangleButton.mousePressed(triangleType);
+
+    sawtoothButton = p.createButton("Sawtooth");
+    sawtoothButton.position(1200, 75);
+    sawtoothButton.mousePressed(sawtoothType);
+
+    squareButton = p.createButton("Square");
+    squareButton.position(1300, 75);
+    squareButton.mousePressed(squareType);
+
+    p.background(51);
+
+    drawBoard();
+  }
+
+  p.draw = function() {
+    vol = slider.value();
+  }
+
+  function drawBoard() {
+    p.push();
+    p.fill(255);
+    p.textSize(20);
+    p.translate(205, 25);
+    p.text("Volume", 0, 0);
+    p.text("Voice Select", 600, 0);
+    p.pop();
+
+    p.push();
+    p.strokeWeight(4);
+    p.stroke(255);
+    p.line(650, 50, 1050, 50);
+    p.pop();
+
+    slider = p.createSlider(0.01, 1, 0.5, 0.01);
+    slider.position(500, 40);
+  }
+}
+
+// Create Sketch
+let myp5 = new p5(board);

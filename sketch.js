98bdeea5ca8dcb54d030109e35eeb50daac834bb
ethan-k64 <59579977+ethan-k64@users.noreply.github.com>
2020-08-06 22:50:25 -0400

    Huge Change.
    Changed the way notes are stored by making them in an array so I don't have to have lots of unneeded variables.  This helps a lot with performance.

--- sketch.js
+++ sketch.js
@@ -5,82 +5,11 @@ Please Don't Steal My Code
 
 // Global Variables-----------------------------------------------------------------------------
 const len = 21;
+const keys = 36;
 let type = "sine";
 
-let notew;
-let note;
-
-let qw;
-let q;
-let ww;
-let w;
-let ew;
-let e;
-let rw;
-let r;
-let tw;
-let t;
-let yw;
-let y;
-let uw;
-let u;
-let iw;
-let i;
-let ow;
-let o;
-let pw;
-let p;
-let zw;
-let z;
-let xw;
-let x;
-let cw;
-let c;
-let vw;
-let v;
-let bw;
-let b;
-let nw;
-let n;
-let mw;
-let m;
-let commaw;
-let comma;
-let periodw;
-let period;
-let slashw;
-let slash;
-
-let twow;
-let two;
-let threew;
-let three;
-let fivew;
-let five;
-let sixw;
-let six;
-let sevenw;
-let seven;
-let ninew;
-let nine;
-let zerow;
-let zero;
-let sw;
-let s;
-let dw;
-let d;
-let fw;
-let f;
-let hw;
-let h;
-let jw;
-let j;
-let lw;
-let l;
-let semicolinw;
-let semicolin;
-let quotew;
-let quote;
+let env = [];
+let osc = [];
 
 let vol = 0;
 let keySustain;
@@ -88,11 +17,24 @@ let keySustain;
 function setup() {
   const cnv = createCanvas(len * 60, 300);
   cnv.position(displayWidth / 2 - width / 2, 175);
+  
+  for (let i = 0; i < keys; i++) {
+    env.push(new p5.Envelope());
+    env[i].setADSR(0, keySustain, 0.2, 0);
+    env[i].setRange(vol, 0);
+    osc.push(new p5.Oscillator());
+    osc[i].amp(env[i]);
+  }
 }
 
 function draw() {
   background(220);
-
+  
+  for (let i = 0; i < keys; i++) {
+    env[i].setADSR(0, keySustain, 0.2, 0);
+    env[i].setRange(vol, 0);
+    osc[i].amp(env[i]);
+  }
   
   drawKeys();
   
@@ -102,528 +44,213 @@ function draw() {
 function keyPressed() {
   // White Keys---------------------------------------------------------------------------------
   if (key == 'q') {
-    q = new p5.Envelope();
-    q.setADSR(0, keySustain, 0.2, 0);
-    q.setRange(vol, 0);
-    q.play();
-
-    qw = new p5.Oscillator();
-    qw.setType(type);
-    qw.start();
-    qw.freq(130);
-    qw.amp(q);
-
-    q.play();
+    osc[0].start();
+    osc[0].freq(130);
+    env[0].play();
   }
 
   if (key == 'w') {
-    w = new p5.Envelope();
-    w.setADSR(0, keySustain, 0.2, 0);
-    w.setRange(vol, 0);
-    w.play();
-
-    ww = new p5.Oscillator();
-    ww.setType(type);
-    ww.start();
-    ww.freq(146);
-    ww.amp(w);
-
-    w.play();
+    osc[1].start();
+    osc[1].freq(146);
+    env[1].play();
   }
 
   if (key == 'e') {
-    e = new p5.Envelope();
-    e.setADSR(0, keySustain, 0.2, 0);
-    e.setRange(vol, 0);
-    e.play();
-
-    ew = new p5.Oscillator();
-    ew.setType(type);
-    ew.start();
-    ew.freq(164);
-    ew.amp(e);
-
-    e.play();
+    osc[2].start();
+    osc[2].freq(164);
+    env[2].play();
   }
 
   if (key == 'r') {
-    r = new p5.Envelope();
-    r.setADSR(0, keySustain, 0.2, 0);
-    r.setRange(vol, 0);
-    r.play();
-
-    rw = new p5.Oscillator();
-    rw.setType(type);
-    rw.start();
-    rw.freq(174);
-    rw.amp(r);
-
-    r.play();
+    osc[3].start();
+    osc[3].freq(174);
+    env[3].play();
   }
 
   if (key == 't') {
-    t = new p5.Envelope();
-    t.setADSR(0, keySustain, 0.2, 0);
-    t.setRange(vol, 0);
-    t.play();
-
-    tw = new p5.Oscillator();
-    tw.setType(type);
-    tw.start();
-    tw.freq(196);
-    tw.amp(t);
-
-    t.play();
+    osc[4].start();
+    osc[4].freq(196);
+    env[4].play();
   }
 
   if (key == 'y') {
-    y = new p5.Envelope();
-    y.setADSR(0, keySustain, 0.2, 0);
-    y.setRange(vol, 0);
-    y.play();
-
-    yw = new p5.Oscillator();
-    yw.setType(type);
-    yw.start();
-    yw.freq(220);
-    yw.amp(y);
-
-    y.play();
+    osc[5].start();
+    osc[5].freq(220);
+    env[5].play();
   }
 
   if (key == 'u') {
-    u = new p5.Envelope();
-    u.setADSR(0, keySustain, 0.2, 0);
-    u.setRange(vol, 0);
-    u.play();
-
-    uw = new p5.Oscillator();
-    uw.setType(type);
-    uw.start();
-    uw.freq(246);
-    uw.amp(u);
-
-    u.play();
+    osc[6].start();
+    osc[6].freq(246);
+    env[6].play();
   }
 
   if (key == 'i') {
-    i = new p5.Envelope();
-    i.setADSR(0, keySustain, 0.2, 0);
-    i.setRange(vol, 0);
-    i.play();
-
-    iw = new p5.Oscillator();
-    iw.setType(type);
-    iw.start();
-    iw.freq(261);
-    iw.amp(i);
-
-    i.play();
+    osc[7].start();
+    osc[7].freq(261);
+    env[7].play();
   }
 
   if (key == 'o') {
-    o = new p5.Envelope();
-    o.setADSR(0, keySustain, 0.2, 0);
-    o.setRange(vol, 0);
-    o.play();
-
-    ow = new p5.Oscillator();
-    ow.setType(type);
-    ow.start();
-    ow.freq(293);
-    ow.amp(o);
-
-    o.play();
+    osc[8].start();
+    osc[8].freq(293);
+    env[8].play();
   }
 
   if (key == 'p') {
-    p = new p5.Envelope();
-    p.setADSR(0, keySustain, 0.2, 0);
-    p.setRange(vol, 0);
-    p.play();
-
-    pw = new p5.Oscillator();
-    pw.setType(type);
-    pw.start();
-    pw.freq(329);
-    pw.amp(p);
-
-    p.play();
+    osc[9].start();
+    osc[9].freq(329);
+    env[9].play();
   }
 
   if (key == 'z') {
-    z = new p5.Envelope();
-    z.setADSR(0, keySustain, 0.2, 0);
-    z.setRange(vol, 0);
-    z.play();
-
-    zw = new p5.Oscillator();
-    zw.setType(type);
-    zw.start();
-    zw.freq(349);
-    zw.amp(z);
-
-    z.play();
+    osc[10].start();
+    osc[10].freq(349);
+    env[10].play();
   }
 
   if (key == 'x') {
-    x = new p5.Envelope();
-    x.setADSR(0, keySustain, 0.2, 0);
-    x.setRange(vol, 0);
-    x.play();
-
-    xw = new p5.Oscillator();
-    xw.setType(type);
-    xw.start();
-    xw.freq(391);
-    xw.amp(x);
-
-    x.play();
+    osc[11].start();
+    osc[11].freq(391);
+    env[11].play();
   }
 
   if (key == 'c') {
-    c = new p5.Envelope();
-    c.setADSR(0, keySustain, 0.2, 0);
-    c.setRange(vol, 0);
-    c.play();
-
-    cw = new p5.Oscillator();
-    cw.setType(type);
-    cw.start();
-    cw.freq(440);
-    cw.amp(c);
-
-    c.play();
+    osc[12].start();
+    osc[12].freq(440);
+    env[12].play();
   }
 
   if (key == 'v') {
-    v = new p5.Envelope();
-    v.setADSR(0, keySustain, 0.2, 0);
-    v.setRange(vol, 0);
-    v.play();
-
-    vw = new p5.Oscillator();
-    vw.setType(type);
-    vw.start();
-    vw.freq(493);
-    vw.amp(v);
-
-    v.play();
+    osc[13].start();
+    osc[13].freq(493);
+    env[13].play();
   }
 
   if (key == 'b') {
-    b = new p5.Envelope();
-    b.setADSR(0, keySustain, 0.2, 0);
-    b.setRange(vol, 0);
-    b.play();
-
-    bw = new p5.Oscillator();
-    bw.setType(type);
-    bw.start();
-    bw.freq(523);
-    bw.amp(b);
-
-    b.play();
+    osc[14].start();
+    osc[14].freq(523);
+    env[14].play();
   }
 
   if (key == 'n') {
-    n = new p5.Envelope();
-    n.setADSR(0, keySustain, 0.2, 0);
-    n.setRange(vol, 0);
-    n.play();
-
-    nw = new p5.Oscillator();
-    nw.setType(type);
-    nw.start();
-    nw.freq(587);
-    nw.amp(n);
-
-    n.play();
+    osc[15].start();
+    osc[15].freq(587);
+    env[15].play();
   }
 
   if (key == 'm') {
-    m = new p5.Envelope();
-    m.setADSR(0, keySustain, 0.2, 0);
-    m.setRange(vol, 0);
-    m.play();
-
-    mw = new p5.Oscillator();
-    mw.setType(type);
-    mw.start();
-    mw.freq(659);
-    mw.amp(m);
-
-    m.play();
+    osc[16].start();
+    osc[16].freq(659);
+    env[16].play();
   }
 
   if (key == ',') {
-    comma = new p5.Envelope();
-    comma.setADSR(0, keySustain, 0.2, 0);
-    comma.setRange(vol, 0);
-    comma.play();
-
-    commaw = new p5.Oscillator();
-    commaw.setType(type);
-    commaw.start();
-    commaw.freq(698);
-    commaw.amp(comma);
-
-    comma.play();
+    osc[17].start();
+    osc[17].freq(698);
+    env[17].play();
   }
 
   if (key == '.') {
-    period = new p5.Envelope();
-    period.setADSR(0, keySustain, 0.2, 0);
-    period.setRange(vol, 0);
-    period.play();
-
-    periodw = new p5.Oscillator();
-    periodw.setType(type);
-    periodw.start();
-    periodw.freq(783);
-    periodw.amp(period);
-
-    period.play();
+    osc[18].start();
+    osc[18].freq(783);
+    env[18].play();
   }
 
   if (key == '/') {
-    slash = new p5.Envelope();
-    slash.setADSR(0, keySustain, 0.2, 0);
-    slash.setRange(vol, 0);
-    slash.play();
-
-    slashw = new p5.Oscillator();
-    slashw.setType(type);
-    slashw.start();
-    slashw.freq(880);
-    slashw.amp(slash);
-
-    slash.play();
+    osc[19].start();
+    osc[19].freq(880);
+    env[19].play();
   }
 
   if (key == '2') {
-    two = new p5.Envelope();
-    two.setADSR(0, keySustain, 0.2, 0);
-    two.setRange(vol, 0);
-    two.play();
-
-    twow = new p5.Oscillator();
-    twow.setType(type);
-    twow.start();
-    twow.freq(138);
-    twow.amp(two);
-
-    two.play();
+    osc[20].start();
+    osc[20].freq(138);
+    env[20].play();
   }
 
   if (key == '3') {
-    three = new p5.Envelope();
-    three.setADSR(0, keySustain, 0.2, 0);
-    three.setRange(vol, 0);
-    three.play();
-
-    threew = new p5.Oscillator();
-    threew.setType(type);
-    threew.start();
-    threew.freq(155);
-    threew.amp(three);
-
-    three.play();
+    osc[21].start();
+    osc[21].freq(155);
+    env[21].play();
   }
 
   if (key == '5') {
-    five = new p5.Envelope();
-    five.setADSR(0, keySustain, 0.2, 0);
-    five.setRange(vol, 0);
-    five.play();
-
-    fivew = new p5.Oscillator();
-    fivew.setType(type);
-    fivew.start();
-    fivew.freq(185);
-    fivew.amp(five);
-
-    five.play();
+    osc[22].start();
+    osc[22].freq(185);
+    env[22].play();
   }
 
   if (key == '6') {
-    six = new p5.Envelope();
-    six.setADSR(0, keySustain, 0.2, 0);
-    six.setRange(vol, 0);
-    six.play();
-
-    sixw = new p5.Oscillator();
-    sixw.setType(type);
-    sixw.start();
-    sixw.freq(207);
-    sixw.amp(six);
-
-    six.play();
+    osc[23].start();
+    osc[23].freq(207);
+    env[23].play();
   }
 
   if (key == '7') {
-    seven = new p5.Envelope();
-    seven.setADSR(0, keySustain, 0.2, 0);
-    seven.setRange(vol, 0);
-    seven.play();
-
-    sevenw = new p5.Oscillator();
-    sevenw.setType(type);
-    sevenw.start();
-    sevenw.freq(233);
-    sevenw.amp(seven);
-
-    seven.play();
+    osc[24].start();
+    osc[24].freq(233);
+    env[24].play();
   }
 
   if (key == '9') {
-    nine = new p5.Envelope();
-    nine.setADSR(0, keySustain, 0.2, 0);
-    nine.setRange(vol, 0);
-    nine.play();
-
-    ninew = new p5.Oscillator();
-    ninew.setType(type);
-    ninew.start();
-    ninew.freq(277);
-    ninew.amp(nine);
-
-    nine.play();
+    osc[25].start();
+    osc[25].freq(277);
+    env[25].play();
   }
 
   if (key == '0') {
-    zero = new p5.Envelope();
-    zero.setADSR(0, keySustain, 0.2, 0);
-    zero.setRange(vol, 0);
-    zero.play();
-
-    zerow = new p5.Oscillator();
-    zerow.setType(type);
-    zerow.start();
-    zerow.freq(311);
-    zerow.amp(zero);
-
-    zero.play();
+    osc[26].start();
+    osc[26].freq(311);
+    env[26].play();
   }
 
   if (key == 's') {
-    s = new p5.Envelope();
-    s.setADSR(0, keySustain, 0.2, 0);
-    s.setRange(vol, 0);
-    s.play();
-
-    sw = new p5.Oscillator();
-    sw.setType(type);
-    sw.start();
-    sw.freq(369.99);
-    sw.amp(s);
-
-    s.play();
+    osc[27].start();
+    osc[27].freq(369.99);
+    env[27].play();
   }
 
   if (key == 'd') {
-    d = new p5.Envelope();
-    d.setADSR(0, keySustain, 0.2, 0);
-    d.setRange(vol, 0);
-    d.play();
-
-    dw = new p5.Oscillator();
-    dw.setType(type);
-    dw.start();
-    dw.freq(415);
-    dw.amp(d);
-
-    d.play();
+    osc[28].start();
+    osc[28].freq(415);
+    env[28].play();
   }
 
   if (key == 'f') {
-    f = new p5.Envelope();
-    f.setADSR(0, keySustain, 0.2, 0);
-    f.setRange(vol, 0);
-    f.play();
-
-    fw = new p5.Oscillator();
-    fw.setType(type);
-    fw.start();
-    fw.freq(466);
-    fw.amp(f);
-
-    f.play();
+    osc[29].start();
+    osc[29].freq(466);
+    env[29].play();
   }
 
   if (key == 'h') {
-    h = new p5.Envelope();
-    h.setADSR(0, keySustain, 0.2, 0);
-    h.setRange(vol, 0);
-    h.play();
-
-    hw = new p5.Oscillator();
-    hw.setType(type);
-    hw.start();
-    hw.freq(554);
-    hw.amp(h);
-
-    h.play();
+    osc[30].start();
+    osc[30].freq(554);
+    env[30].play();
   }
 
   if (key == 'j') {
-    j = new p5.Envelope();
-    j.setADSR(0, keySustain, 0.2, 0);
-    j.setRange(vol, 0);
-    j.play();
-
-    jw = new p5.Oscillator();
-    jw.setType(type);
-    jw.start();
-    jw.freq(622);
-    jw.amp(j);
-
-    j.play();
+    osc[31].start();
+    osc[31].freq(622);
+    env[31].play();
   }
 
   if (key == 'l') {
-    l = new p5.Envelope();
-    l.setADSR(0, keySustain, 0.2, 0);
-    l.setRange(vol, 0);
-    l.play();
-
-    lw = new p5.Oscillator();
-    lw.setType(type);
-    lw.start();
-    lw.freq(739.99);
-    lw.amp(l);
-
-    l.play();
+    osc[32].start();
+    osc[32].freq(739.99);
+    env[32].play();
   }
 
   if (key == ';') {
-    semicolin = new p5.Envelope();
-    semicolin.setADSR(0, keySustain, 0.2, 0);
-    semicolin.setRange(vol, 0);
-    semicolin.play();
-
-    semicolinw = new p5.Oscillator();
-    semicolinw.setType(type);
-    semicolinw.start();
-    semicolinw.freq(830);
-    semicolinw.amp(semicolin);
-
-    semicolin.play();
+    osc[33].start();
+    osc[33].freq(830);
+    env[33].play();
   }
 
   if (key == '\'') {
-    quote = new p5.Envelope();
-    quote.setADSR(0, keySustain, 0.2, 0);
-    quote.setRange(vol, 0);
-    quote.play();
-
-    quotew = new p5.Oscillator();
-    quotew.setType(type);
-    quotew.start();
-    quotew.freq(932);
-    quotew.amp(quote);
-
-    quote.play();
+    osc[34].start();
+    osc[34].freq(932);
+    env[34].play();
   }
 }
 
@@ -733,12 +360,6 @@ function drawKeys() {
   text(";", 1136, 50);
   text("'", 1197, 50);
   pop();
-}
-
-function press(frequency) {
-  notew.freq(frequency);
-  notew.start();
-  note.play();
 }
 
 function sineType() {
